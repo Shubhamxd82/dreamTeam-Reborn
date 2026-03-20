502ba7ecf05a1612e3f8a08cd440f07b28099d10
SUBHAJIT DAS <119496423+Shubhamxd82@users.noreply.github.com>
2026-03-20 17:06:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -119,33 +119,45 @@ class Config(object):
 
     # ==================== TEXT TEMPLATES ====================
     ABOUT_BOT_TEXT = f"""
-Hello, I'm a Permanent File Store Bot.
+Hello, I'm Ryan Gosling - I Drive.
+AS i Said, Send me any File & It will be uploaded in My Database & You will Get the File Link Which **Never Expires**. 
 
-╭────[ **🔅Bot Info🔅** ]────⍟
+╭────[ **🔅Literally Me🔅**]────⍟
 │
-├🔸🤖 **My Name:** [𝐅𝐢𝐥𝐞 𝐒𝐭𝐨𝐫𝐞](https://t.me/{BOT_USERNAME})
+├🔸🤖 **My Name:** [𝐖𝐨𝐧𝐝𝐞𝐫 𝐌𝐚𝐧](https://t.me/{BOT_USERNAME})
 │
 ├🔸📝 **Language:** [𝐏𝐲𝐭𝐡𝐨𝐧𝟑](https://www.python.org)
 │
 ├🔹📚 **Library:** [𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦](https://docs.pyrogram.org)
 │
+├🔹📡 **Hosted On:** [𝐕𝐏𝐒](http://vultr.com/)
+│
+├🔸👨‍💻 **Owner:** [𝐒𝐡𝐮𝐛𝐡𝐚𝐦](https://t.me/Nexus_Shubhu) 
+│
+├🔹©️ **Powered By:** [𝐌𝐨𝐯𝐢𝐞𝐬𝐬𝐬𝟒𝐞𝐫𝐬](https://t.me/Moviesss4ers)
+│
 ╰──────[ 😎 ]───────────⍟
 """
+	ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [【﻿Dengerous】](https://t.me/Dengerous53) 
 
-    ABOUT_DEV_TEXT = """
-🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [【﻿Dengerous】](https://t.me/Dengerous53)
+𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐢𝐬 𝐒𝐮𝐩𝐞𝐫 𝐍𝐨𝐨𝐛. 𝐉𝐮𝐬𝐭 𝐋𝐞𝐚𝐫𝐧𝐢𝐧𝐠 𝐟𝐫𝐨𝐦 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐃𝐨𝐜𝐬. 𝐀𝐧𝐝 𝐒𝐞𝐞𝐤𝐢𝐧𝐠 𝐇𝐞𝐥𝐩 𝐅𝐫𝐨𝐦 𝐏𝐫𝐨 𝐂𝐨𝐝𝐞𝐫𝐬\n**@Nexus_Shubhu**
 
-**@Nexus_Shubhu**
+𝐀𝐥𝐬𝐨 𝐫𝐞𝐦𝐞𝐦𝐛𝐞𝐫 𝐭𝐡𝐚𝐭 𝐝𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐰𝐢𝐥𝐥 𝐃𝐞𝐥𝐞𝐭𝐞 𝐀𝐝𝐮𝐥𝐭 𝐂𝐨𝐧𝐭𝐞𝐧𝐭𝐬 𝐟𝐫𝐨𝐦 𝐃𝐚𝐭𝐚𝐛𝐚𝐬𝐞. 𝐒𝐨 𝐛𝐞𝐭𝐭𝐞𝐫 𝐝𝐨𝐧'𝐭 𝐒𝐭𝐨𝐫𝐞 𝐓𝐡𝐨𝐬𝐞 𝐊𝐢𝐧𝐝 𝐨𝐟 𝐓𝐡𝐢𝐧𝐠𝐬.
+
 """
+	HOME_TEXT = """
+Hello, [{}](tg://user?id={})\n\nThis is a Literally Permanent **FileStore Bot**.
 
-    HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+How to Use Bot & it's Benefits??
 
-📢 Send me any File & I'll store it and give you a permanent link.
+📢 Send me any File & It will be uploaded in My Database & You will Get the File Link Which Never Expires.
 
-⚠️ If bot gets banned, your links still work with the new bot!
+⚠️ **Benefits**: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue.
 
-❌ **PORNOGRAPHY CONTENTS** are strictly prohibited.
+🗣️ **For Example**: If Bot Get Banned & Deleted, Your All Links will Redirect to New Bot & All Files Are Still Accessable, Dont Worry You Don't have to Make any Changes Anywhere Because We Do 😎
+
+❌ **PORNOGRAPHY CONTENTS** are strictly prohibited & get Permanent Ban.
 """
 
     FORCE_SUB_TEXT = """
