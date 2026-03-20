6f16214f7cef2e52b399eec717eb9fa07f0fd0de
SUBHAJIT DAS <119496423+Shubhamxd82@users.noreply.github.com>
2026-03-20 17:54:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -140,7 +140,7 @@ AS i Said, Send me any File & It will be uploaded in My Database & You will Get 
 │
 ╰──────[ 😎 ]───────────⍟
 """
-	ABOUT_DEV_TEXT = f"""
+    ABOUT_DEV_TEXT = f"""
 🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [【﻿Dengerous】](https://t.me/Dengerous53) 
 
 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐢𝐬 𝐒𝐮𝐩𝐞𝐫 𝐍𝐨𝐨𝐛. 𝐉𝐮𝐬𝐭 𝐋𝐞𝐚𝐫𝐧𝐢𝐧𝐠 𝐟𝐫𝐨𝐦 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐃𝐨𝐜𝐬. 𝐀𝐧𝐝 𝐒𝐞𝐞𝐤𝐢𝐧𝐠 𝐇𝐞𝐥𝐩 𝐅𝐫𝐨𝐦 𝐏𝐫𝐨 𝐂𝐨𝐝𝐞𝐫𝐬\n**@Nexus_Shubhu**
@@ -148,7 +148,7 @@ AS i Said, Send me any File & It will be uploaded in My Database & You will Get 
 𝐀𝐥𝐬𝐨 𝐫𝐞𝐦𝐞𝐦𝐛𝐞𝐫 𝐭𝐡𝐚𝐭 𝐝𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐰𝐢𝐥𝐥 𝐃𝐞𝐥𝐞𝐭𝐞 𝐀𝐝𝐮𝐥𝐭 𝐂𝐨𝐧𝐭𝐞𝐧𝐭𝐬 𝐟𝐫𝐨𝐦 𝐃𝐚𝐭𝐚𝐛𝐚𝐬𝐞. 𝐒𝐨 𝐛𝐞𝐭𝐭𝐞𝐫 𝐝𝐨𝐧'𝐭 𝐒𝐭𝐨𝐫𝐞 𝐓𝐡𝐨𝐬𝐞 𝐊𝐢𝐧𝐝 𝐨𝐟 𝐓𝐡𝐢𝐧𝐠𝐬.
 
 """
-	HOME_TEXT = """
+    HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Literally Permanent **FileStore Bot**.
 
 How to Use Bot & it's Benefits??
